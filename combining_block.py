c21b54264e21365d45070a053d893c0b5a81a3b9
NetheriteStriker <NetheriteStriker@gmail.com>
2026-05-17 22:19:00 -0700

    smily_face

--- combining_block.py
+++ combining_block.py
@@ -5,4 +5,23 @@ def checkerboard():
   vm.moveTo(2, Direction.DOWN)
   vm.createText('8', 58, False, Block.BLUE_WOOL)
     
+
+def smily_face():
+  vm.createCircle(30, True, [dict(GROUND=True, TYPE=Block.YELLOW_WOOL)])
+  vm.createCircle(30, False, [dict(GROUND=True, TYPE=Block.BLACK_WOOL)])
+  vm.moveTo(10, Direction.FORWARD)
+  vm.moveTo(15, Direction.RIGHT)
+  vm.createCircle(5, True, [dict(GROUND=True, TYPE=Block.LIGHT_BLUE_WOOL)])
+  vm.createCircle(5, False, [dict(GROUND=True, TYPE=Block.BLACK_WOOL)])
+  vm.moveTo(25, Direction.LEFT)
+  vm.createCircle(6, True, [dict(GROUND=True, TYPE=Block.LIGHT_BLUE_WOOL)])
+  vm.createCircle(6, False, [dict(GROUND=True, TYPE=Block.BLACK_WOOL)])
+  vm.moveTo(5, Direction.RIGHT)
+  vm.moveTo(12, Direction.BACKWARD)
+  vm.createSquare(6, True, [dict(GROUND=True, TYPE=Block.WHITE_WOOL)])
+  vm.createSquare(6, False, [dict(GROUND=True, TYPE=Block.BLACK_WOOL)])
+  vm.moveTo(12, Direction.BACKWARD)
+  vm.createArc(7, 7, 180, True,[dict(GROUND=True, TYPE=Block.RED_WOOL), dict(GROUND=True, TYPE=Block.BLACK_WOOL)])
+  vm.createArc(7, 7, 180, False, [dict(GROUND=True, TYPE=Block.BLACK_WOOL)])
+
   
